6203356e03889ec434706a8cc8e6c02bc3bd411d
encodedBicoding <dominicisioma000@gmail.com>
2021-05-12 01:23:26 +0100

    add some assets

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,7 +4,11 @@
     <header id="header" class="fixed-top d-flex align-items-center">
       <div class="container d-flex justify-content-between align-items-center">
         <div class="logo">
-          <h1><a href="index.html">Koogah</a></h1>
+          <h1>
+            <a href="index.html">
+              <img src="../assets/img/app_logo.svg" alt="Koogah Logo"/>
+            </a>
+            </h1>
           <!-- Uncomment below if you prefer to use an image logo -->
           <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
         </div>
