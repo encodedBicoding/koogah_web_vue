16920ae79a77b8dcad1f82787003752261abd86d
derinsola16 <aderinsolaodusany@gmail.com>
2021-05-24 22:53:31 +0100

    register design

---
+++ src/components/Footer.vue
@@ -0,0 +1,56 @@
+<template>
+  <!-- ======= Footer ======= -->
+  <footer class="footer" role="contentinfo">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4 mb-4 mb-md-0">
+          <h3 class="text-white">About Koogah</h3>
+          <p>
+            Koogah is a mobile-first logistics and courier service platform,
+            with the aim to solve same day issues, while giving our customers
+            ability to be with their packages from the comfort of their homes.
+          </p>
+          <p class="social">
+            <a href="#" class="me-2"><span class="bi bi-twitter"></span></a>
+            <a href="#" class="me-2"><span class="bi bi-facebook"></span></a>
+            <a href="#" class="me-2"><span class="bi bi-instagram"></span></a>
+            <a href="#" class="me-2"><span class="bi bi-linkedin"></span></a>
+          </p>
+        </div>
+        <div class="col-md-7 ms-auto">
+          <div class="row site-section pt-0">
+            <div class="col-md-4 mb-4 mb-md-0">
+              <h3 class="text-white">Navigation</h3>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#" class="text-white">Become a Dispatcher</a>
+                </li>
+                <li><a href="#" class="text-white">FAQ</a></li>
+                <li><a href="#" class="text-white">Contact</a></li>
+              </ul>
+            </div>
+            <div class="col-md-4 mb-4 mb-md-0">
+              <h3 class="text-white">Services</h3>
+              <ul class="list-unstyled">
+                <li><a href="#" class="text-white">Team</a></li>
+                <li><a href="#" class="text-white">Collaboration</a></li>
+                <li><a href="#" class="text-white">Events</a></li>
+              </ul>
+            </div>
+            <div class="col-md-4 mb-4 mb-md-0">
+              <h3 class="text-white">Downloads</h3>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#" class="text-white">Get from the App Store</a>
+                </li>
+                <li>
+                  <a href="#" class="text-white">Get from the Play Store</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
